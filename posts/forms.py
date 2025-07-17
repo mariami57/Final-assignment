d6f39c131ab9f8c96eeaf54c7dcf4327dc4e8311
mariami57 <mmitova3@gmail.com>
2025-07-17 15:20:20 +0300

    updated add_post template, view and form

--- posts/forms.py
+++ posts/forms.py
@@ -1,28 +1,30 @@
 from django import forms
 
+
 from books.models import Book
 from destinations.models import Destination
 from posts.models import Post
 
 
 class PostBaseForm(forms.ModelForm):
+    book_choice = forms.ChoiceField(label='Choose a book', required=False)
+    book_title = forms.CharField(label='Enter a book title', required=False)
+
+    destination_choice = forms.ChoiceField(label='Choose a destination', required=False)
+    destination_name = forms.CharField(label='Or enter a destination name', required=False)
+
     class Meta:
         model = Post
-        book = forms.ModelChoiceField(queryset=Book.objects.all())
-        destination = forms.ModelChoiceField(queryset=Destination.objects.all())
-        fields = ['title', 'book', 'destination', 'content','image1', 'image2', 'image3']
-
+        fields = ['title', 'book_title', 'destination_name', 'content','image1', 'image2', 'image3']
         labels = {
             'title': 'Title:',
-            'book': 'Book:',
-            'destination': 'Destination:',
             'content': 'Content:',
-            'book_review': 'Book Review:',
             'image1': 'First Image:',
             'image2': 'Second Image:',
             'image3': 'Third Image:',
 
         }
+
 
         widgets = {
             'title':forms.TextInput(attrs={}),
@@ -31,6 +33,15 @@ class PostBaseForm(forms.ModelForm):
             'image2': forms.ClearableFileInput(attrs={'id': 'custom-upload2', 'class': 'no-border', 'style': 'border: none;'}),
             'image3': forms.ClearableFileInput(attrs={'id': 'custom-upload3', 'class': 'no-border', 'style': 'border: none;'}),
         }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        books = Book.objects.all().values_list('id', 'title')
+        self.fields['book_choice'].choices = list(books) + [("other", "Other")]
+
+        destinations = Destination.objects.all().values_list('id', 'name')
+        self.fields['destination_choice'].choices = list(destinations) + [("other", "Other")]
+
 
 class PostCreateForm(PostBaseForm):
     pass
@@ -38,5 +49,3 @@ class PostCreateForm(PostBaseForm):
 class PostEditForm(PostBaseForm):
     pass
 
-class PostDeleteForm(PostBaseForm):
-    pass
--- posts/views.py
+++ posts/views.py
@@ -6,7 +6,9 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView
 
+from books.models import Book
 from common.mixins import UserIsCreatorMixin
+from destinations.models import Destination
 from posts.forms import PostCreateForm, PostEditForm
 from posts.models import Post
 
@@ -24,8 +26,47 @@ class AddPostView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.user = self.request.user
+
+        # Handle book
+        book_choice = form.cleaned_data.get('book_choice')
+        if book_choice == "other":
+            title = form.cleaned_data.get('book_title')
+            if title:
+                book, _ = Book.objects.get_or_create(
+                    title=title,
+                    defaults={'author': 'Unknown', 'genre': None, 'added_by': self.request.user}
+                )
+            else:
+                book = None
+        elif book_choice:
+            book = Book.objects.get(id=book_choice)
+        else:
+            book = None
+        form.instance.book = book
+
+        # Handle destination
+        dest_choice = form.cleaned_data.get('destination_choice')
+        if dest_choice == "other":
+            name = form.cleaned_data.get('destination_name')
+            if name:
+                destination, _ = Destination.objects.get_or_create(
+                    name=name,
+                    defaults={'country': 'Unknown'}
+                )
+            else:
+                destination = None
+        elif dest_choice:
+            destination = Destination.objects.get(id=dest_choice)
+        else:
+            destination = None
+        form.instance.destination = destination
+
         response = super().form_valid(form)
-        messages.success(self.request, "Post created! Want to leave a book review?")
+
+        if book and destination:
+            book.destinations.add(destination)
+
+        messages.success(self.request, 'Post created! Want to leave a book review?')
         return response
 
 class PostEditView(LoginRequiredMixin, UserIsCreatorMixin, UpdateView):

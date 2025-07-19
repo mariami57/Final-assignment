9c92506f92823f6fcb4b17a49a7c4f28fdfbf2b3
mariami57 <mmitova3@gmail.com>
2025-07-19 16:05:20 +0300

    updated the add post form

--- posts/forms.py
+++ posts/forms.py
@@ -7,11 +7,16 @@ from posts.models import Post
 
 
 class PostBaseForm(forms.ModelForm):
-    book_choice = forms.ChoiceField(label='Choose a book', required=False)
-    destination_choice = forms.ChoiceField(label='Choose a destination', required=False)
+    book_choice = forms.CharField(label='Book', required=False)
+    book_title = forms.CharField(label='Book Title', required=False)
+
+    destination_choice = forms.CharField(label='Destination', required=False)
+    destination_name = forms.CharField(label='Destination Name', required=False)
+
+
     class Meta:
         model = Post
-        fields = ['title', 'book_choice', 'destination_choice', 'content','image1', 'image2', 'image3']
+        fields = ['title', 'book_choice', 'book_title','destination_choice', 'destination_name', 'content','image1', 'image2', 'image3']
         labels = {
             'title': 'Title:',
             'content': 'Content:',
@@ -35,23 +40,31 @@ class PostBaseForm(forms.ModelForm):
 
 
         books = Book.objects.all().values_list('id', 'title')
-        self.fields['book_choice'].choices = [('', '--- Select a book ---')] + list(books)
+        self.fields['book_choice'].choices = [('', '--- Select a book ---')] + list(books) + [("other", "Other")]
 
 
         destinations = Destination.objects.all().values_list('id', 'name')
-        self.fields['destination_choice'].choices = [('', '--- Select a destination ---')] + list(destinations)
+        self.fields['destination_choice'].choices = [('', '--- Select a destination ---')] + list(destinations) + [("other", "Other")]
 
+        if self.data:
+            self.fields['book_choice'].initial = self.data.get('book_choice')
+            self.fields['book_title'].initial = self.data.get('book_title')
+
+            self.fields['destination_choice'].initial = self.data.get('destination_choice')
+            self.fields['destination_name'].initial = self.data.get('destination_name')
 
 
 
     def clean(self):
         cleaned_data = super().clean()
         book_choice = cleaned_data.get('book_choice')
+        book_title = cleaned_data.get('book_title')
 
         dest_choice = cleaned_data.get('destination_choice')
+        dest_name = cleaned_data.get('destination_name')
 
-        if not book_choice or not dest_choice:
-            raise forms.ValidationError("Please select both book and destination at the same time.")
+        if (not book_choice and not book_title) or (not dest_choice and not dest_name):
+            raise forms.ValidationError("Please select both book and destination.")
 
         return cleaned_data
 

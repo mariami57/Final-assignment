8bfcc1825156b908ba4cc03c8f7fde24965eaa04
mariami57 <mmitova3@gmail.com>
2025-07-26 16:27:59 +0300

    added more links to the book-genre-review logic

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from books.views import GenreBooksListView
+from books.views import GenreBooksListView, BooksByGenreListView
 
 urlpatterns = [
 
-    path('genres-list/', GenreBooksListView.as_view(), name='genres-list'),
+    path('genres/', GenreBooksListView.as_view(), name='genres-list'),
+    path('<path:genre>/', BooksByGenreListView.as_view(), name='books-by-genre'),
 
 ]
--- books/views.py
+++ books/views.py
@@ -1,11 +1,37 @@
-# Create your views here.
+from collections import defaultdict
 from django.views.generic import ListView
-
 from books.models import Book
 
-
-
+# Create your views here.
 class GenreBooksListView(ListView):
     model = Book
     template_name = 'book/genres-list.html'
 
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+
+        grouped_books = defaultdict(list)
+
+        for book in Book.objects.all():
+            genre = book.genre if book.genre is not None else 'Undefined'
+            grouped_books[genre].append(book)
+
+        context['grouped_books'] = dict(grouped_books)
+
+        return context
+
+class BooksByGenreListView(ListView):
+    model = Book
+    template_name = 'book/books-by-genre.html'
+
+    def get_queryset(self):
+        genre_param = self.kwargs['genre']
+        if genre_param.lower() == 'undefined':
+            return Book.objects.filter(genre__isnull=True)
+        return Book.objects.filter(genre=genre_param)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        genre_param = self.kwargs['genre']
+        context['genre'] = 'Undefined' if genre_param.lower() == 'undefined' else genre_param
+        return context

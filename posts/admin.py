3cd8d276272b7855d227dcd8e47d69fd0f46c858
mariami57 <mmitova3@gmail.com>
2025-07-28 13:24:25 +0300

    Registered post and review models

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from posts.models import Post
+
+
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'book', 'destination', 'content', 'user', 'created_at')
+    search_fields = ('title', 'content')
+    list_filter = ('created_at', 'destination', 'book', 'user')
--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from reviews.models import Review
+
+
 # Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('book', 'user', 'content', 'rating', 'date_added')
+    search_fields =('rating', 'content')
+    list_filter = ('book', 'user', 'date_added', 'rating')

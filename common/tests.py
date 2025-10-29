dc52a0494deb3d83f25d4b5e0b9dabe7b022a1e1
mariami57 <mmitova3@gmail.com>
2025-10-29 13:21:48 +0200

    added test

--- common/tests.py
+++ common/tests.py
@@ -1,3 +1,34 @@
+from io import BytesIO
+
+from PIL import Image
+from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 
+from books.models import Book
+from destinations.models import Destination
+from posts.models import Post
+
 # Create your tests here.
+UserModel = get_user_model()
+class ModelTests(TestCase):
+
+    def create_test_image(self, name='test_image.jpg', size=(100, 100), color=(255, 0, 0)):
+        file_obj = BytesIO()
+        image = Image.new('RGB', size=size, color=color)
+        image.save(file_obj, 'JPEG')
+        file_obj.seek(0)
+        return SimpleUploadedFile(name, file_obj.read(), content_type='image/jpeg')
+
+    def setUp(self):
+        test_image = self.create_test_image()
+        self.user = UserModel.objects.create(username='testuser', password='12345')
+        self.dest = Destination.objects.create(name='Alaska', country='USA', created_by=self.user, latitude=63.5888, longitude=154.4931)
+        self.book = Book.objects.create(title='Into the Wild', author='Jon Krakauer', genre='Biography', added_by=self.user)
+        self.post = Post.objects.create(user=self.user, book=self.book, destination=self.dest,
+                    content='It was cold but beautiful!',
+                    title='My Alaska Trip', image1=test_image)
+
+
+    def test_post_str_returns_title(self):
+        self.assertEqual(str(self.post), "My Alaska Trip")
